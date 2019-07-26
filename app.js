5e7beee3efac336712978480f335732e641d23ce
justJosh1004 <j.joson@live.com>
2019-07-26 11:37:20 -0400

    set up CORS config

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,7 +9,7 @@ mongoose
     `mongodb+srv://josh:${
       process.env.MONGO_ATLAS_PW
     }@cluster0-afuh4.mongodb.net/test?retryWrites=true&w=majority`,
-    { userCreateIndex: true, useNewUrlParser: true }
+    { useCreateIndex: true, useNewUrlParser: true }
   )
   .then(() => {
     console.log('Connected to Database!');
@@ -16,5 +17,20 @@ mongoose
   .catch(() => {
     console.log('Connection Failed!');
   });
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
 
 module.exports = app;
